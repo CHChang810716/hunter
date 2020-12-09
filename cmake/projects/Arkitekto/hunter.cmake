950960e8f1d67f7bc64155417f4a8e8ccebbad64
Chia-Hua Chang <CHChang810716@gmail.com>
2020-12-09 22:33:06 +0800

    update arkitekto

--- cmake/projects/Arkitekto/hunter.cmake
+++ cmake/projects/Arkitekto/hunter.cmake
@@ -15,6 +15,12 @@ hunter_add_version(
     URL "https://github.com/CHChang810716/Arkitekto/archive/1.0.0.tar.gz"
     SHA1 "e3a40283ae9ad14d64d0e256d2a3deaf7ca46af7"
 )
+hunter_add_version(
+    PACKAGE_NAME Arkitekto
+    VERSION "1.0.1"
+    URL "https://github.com/CHChang810716/Arkitekto/archive/1.0.1.tar.gz"
+    SHA1 "bb06464603a44a958000d2de78117331c2636e83"
+)
 
 
 hunter_pick_scheme(DEFAULT url_sha1_cmake)
